d6f1045e4d17dafce1704ba7c4285043015c596f
stuff <stuff@fitzpatricksr.us>
2015-08-04 21:13:28 -0700

    First impl of CompositeOrLampPattern

---
+++ NetBeansProjects/JavaApplication2/src/us/cownet/lamps/FadingLampPattern.java
@@ -0,0 +1,25 @@
+package us.cownet.lamps;
+
+public class FadingLampPattern implements LampPattern {
+
+	@Override
+	public byte[] getPattern() {
+	}
+
+	public void setPattern(LampPattern pattern) {
+
+	}
+
+	@Override
+	public byte getColumn(int x) {
+	}
+
+	@Override
+	public boolean getLamp(int x, int y) {
+	}
+
+	@Override
+	public int getColCount() {
+	}
+
+}
