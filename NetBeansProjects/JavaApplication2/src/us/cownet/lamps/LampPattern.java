e9666cc0e9880c2037aaba955eb8c676b8a12dca
fitzpatricksrus <junk@fitzpatircksr.us>
2015-08-23 17:03:52 -0700

    Added default implementation of getColumn so you can define the contents of the pattern easily as either individual lamps or whole columns.

--- NetBeansProjects/JavaApplication2/src/us/cownet/lamps/LampPattern.java
+++ NetBeansProjects/JavaApplication2/src/us/cownet/lamps/LampPattern.java
@@ -14,9 +14,22 @@ package us.cownet.lamps;
  isDone() and reset() are ignored by the lamp matrix.  They are only used
  by pattern containers to tell when to transition from one pattern to the next.
 
+ Subclasses MUST override either getColumn or getLamp(col,row) or they things will
+ just infinately recurse.  Define the pattern contents in terms of entire columns
+ or individual lamps.  Up to you.
+
  */
 public interface LampPattern {
-	public byte getColumn(int x);
+	public default byte getColumn(int x) {
+		byte result = 0;
+		for (int i = 0; i < getColCount(); i++) {
+			result <<= 1;
+			if (getLamp(x, i)) {
+				result |= 1;
+			}
+		}
+		return result;
+	}
 
 	public int getColCount();
 
