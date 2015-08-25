9c424efda782c04a41161205c6f622f8e823b45a
fitzpatricksrus <junk@fitzpatircksr.us>
2015-08-24 21:50:24 -0700

    trying to clean up pinballoutputcontrollertest.  doesn't work.   can't figure out why.

--- NetBeansProjects/JavaApplication2/src/us/cownet/lamps/tests/PinballOutputControllerTest.java
+++ NetBeansProjects/JavaApplication2/src/us/cownet/lamps/tests/PinballOutputControllerTest.java
@@ -3,15 +3,16 @@ package us.cownet.lamps.tests;
 import us.cownet.lamps.PinballOutputController;
 import us.cownet.lamps.wpc.WpcOutputController;
 import us.cownet.testing.Test;
-import us.cownet.timers.Timer;
-import us.cownet.timers.TimerUtil;
 
 public class PinballOutputControllerTest implements Test {
+	private PinballOutputController hardware;
+	private int row;
+	private int col;
+
 	public PinballOutputControllerTest(PinballOutputController testController) {
 		hardware = testController;
 		row = 0;
 		col = 0;
-		ticks = new Timer(delayPeriod);
 	}
 
 	@Override
@@ -20,30 +21,21 @@ public class PinballOutputControllerTest implements Test {
 
 	@Override
 	public void loop() {
-		TimerUtil.INSTANCE.tick();
-		if (ticks.isTime()) {
-			hardware.write(PinballOutputController.Register.LAMP_COL, (byte)0);
-			// set the row lamps
-			hardware.write(PinballOutputController.Register.LAMP_ROW, (byte)(1 << row));
-			// turn the proper column back on
-			hardware.write(PinballOutputController.Register.LAMP_COL, (byte)(1 << col));
+		hardware.write(PinballOutputController.Register.LAMP_COL, (byte)0);
+		// set the row lamps
+		hardware.write(PinballOutputController.Register.LAMP_ROW, (byte)(1 << row));
+		// turn the proper column back on
+		hardware.write(PinballOutputController.Register.LAMP_COL, (byte)(1 << col));
 
-			row = row + 1;
-			if (row >= 8) {
-				row = 0;
-				col = col + 1;
-				if (col >= 8) {
-					col = 0;
-				}
+		row = row + 1;
+		if (row >= 8) {
+			row = 0;
+			col = col + 1;
+			if (col >= 8) {
+				col = 0;
 			}
 		}
 	}
-
-	private static final long delayPeriod = 200L * 1000L;
-	private PinballOutputController hardware;
-	private int row;
-	private int col;
-	private Timer ticks;
 
 	public static PinballOutputController createTestController() {
 		PinballOutputController controller;
